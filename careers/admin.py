a0a9beb693a0e7974a00ab16618f681bb30b1ef4
UdsonWillams111 <udson.rego@invillia.com>
2024-12-07 14:50:16 -0300

    feature: add career admin to project

--- careers/admin.py
+++ careers/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+
 # Register your models here.
+from careers.models import Careers
+
+
+class CareersAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(Careers)  # Register your models here.
